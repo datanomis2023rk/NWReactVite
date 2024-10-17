42e3b020920da596016c134a087f4c37445074f7
datanomis2023rk <rami.korkeila@student.careeria.fi>
2024-10-17 11:08:46 +0300

    eka kommitti

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import "./App.css";
+import Laskuri from "./Laskuri";
+import Viesti from "./Viesti";
+import Posts from "./Posts";
+
+const App = () => {
+  // App komponentin tila
+  const [showLaskuri, setShowLaskuri] = useState(false);
+
+  const huomio = () => {
+    alert("Huomio!");
+  };
+
+  return (
+    <div className="App">
+      <h1>Hello from React!</h1>
+
+      <Posts />
+
+      {showLaskuri && <Laskuri huomio={huomio} />}
+
+      {showLaskuri && (
+        <button onClick={() => setShowLaskuri(!showLaskuri)}>
+          Piilota laskuri
+        </button>
+      )}
+
+      {!showLaskuri && (
+        <button onClick={() => setShowLaskuri(!showLaskuri)}>
+          Näytä laskuri
+        </button>
+      )}
+
+      <Viesti teksti="tervehdys app komponentista" />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/Posts.jsx
@@ -0,0 +1,32 @@
+import "./App.css";
+import React, { useState, useEffect } from "react";
+
+const Posts = () => {
+  // Komponentin tilan määritys
+  const [posts, setPosts] = useState([]);
+  const [showPosts, setShowPosts] = useState(false);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((res) => res.json()) //muutetaan json data javascriptiksi
+      .then((oliot) => setPosts(oliot));
+  }, []);
+
+  return (
+    <>
+      <h2 onClick={() => setShowPosts(!showPosts)}>Posts from typicode</h2>
+
+      {showPosts &&
+        posts &&
+        posts.map((p) => (
+          <div className="posts" key={p.id}>
+            <h4>{p.title}</h4>
+            <h5>User ID: {p.userId}</h5>
+            <p>{p.body}</p>
+          </div>
+        ))}
+    </>
+  );
+};
+
+export default Posts;
