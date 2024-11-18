281d1a77fbc9602b10ca954a9f63984879c96089
datanomis2023rk <rami.korkeila@student.careeria.fi>
2024-11-18 23:39:32 +0200

    Hyvin menee!

--- src/CustomerList.jsx
+++ src/CustomerList.jsx
@@ -3,19 +3,33 @@ import React, { useState, useEffect } from "react";
 import CustomerService from "./services/Customer";
 import Customer from "./Customer";
 import CustomerAdd from "./CustomerAdd";
+import CustomerEdit from "./CustomerEdit";
 
 const CustomerList = ({ setIsPositive, setShowMessage, setMessage }) => {
   // Komponentin tilan määritys
   const [customers, setCustomers] = useState([]);
   const [showCustomers, setShowCustomers] = useState(false);
   const [lisäystila, setLisäystila] = useState(false);
+  const [muokkaustila, setMuokkaustila] = useState(false);
+  const [reload, reloadNow] = useState(false);
+  const [muokattavaCustomer, setMuokattavaCustomer] = useState(false);
+  const [search, setSearch] = useState("");
 
   useEffect(() => {
     CustomerService.getAll().then((data) => {
       setCustomers(data);
     });
-  }, [lisäystila]);
+  }, [lisäystila, muokkaustila, reload]);
 
+  const handleSearchInputChange = (event) => {
+    setShowCustomers(true);
+    setSearch(event.target.value.toLowerCase());
+  };
+
+  const editCustomer = (customer) => {
+    setMuokattavaCustomer(customer);
+    setMuokkaustila(true);
+  };
   return (
     <>
       <h1>
@@ -33,6 +47,14 @@ const CustomerList = ({ setIsPositive, setShowMessage, setMessage }) => {
         )}
       </h1>
 
+      {!lisäystila && !muokkaustila && (
+        <input
+          placeholder="Search by company name"
+          value={search}
+          onChange={handleSearchInputChange}
+        />
+      )}
+
       {lisäystila && (
         <CustomerAdd
           setLisäystila={setLisäystila}
@@ -42,9 +64,37 @@ const CustomerList = ({ setIsPositive, setShowMessage, setMessage }) => {
         />
       )}
 
-      {showCustomers &&
+      {muokkaustila && (
+        <CustomerEdit
+          setMuokkaustila={setMuokkaustila}
+          setIsPositive={setIsPositive}
+          setMessage={setMessage}
+          setShowMessage={setShowMessage}
+          muokattavaCustomer={muokattavaCustomer}
+        />
+      )}
+
+      {!lisäystila &&
+        !muokkaustila &&
+        showCustomers &&
         customers &&
-        customers.map((c) => <Customer key={c.customerId} customer={c} />)}
+        customers.map((c) => {
+          const lowerCaseName = c.companyName.toLowerCase();
+          if (lowerCaseName.indexOf(search) > -1) {
+            return (
+              <Customer
+                key={c.customerId}
+                customer={c}
+                reloadNow={reloadNow}
+                reload={reload}
+                setIsPositive={setIsPositive}
+                setShowMessage={setShowMessage}
+                setMessage={setMessage}
+                editCustomer={editCustomer}
+              />
+            );
+          }
+        })}
     </>
   );
 };
