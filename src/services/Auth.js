4f31581e7aa8d913fec88ebacaadffa84f7bc248
datanomis2023rk <rami.korkeila@student.careeria.fi>
2025-04-11 16:32:03 +0300

    backendin uusi osoite

--- src/services/Auth.js
+++ src/services/Auth.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 //const baseUrl = "https://localhost:7229/api/authentication";
 const baseUrl =
-  "https://northwindrestapi-gmccb0cxcfgcg0a5.northeurope-01.azurewebsites.net";
+  "https://northwindrestapi-gmccb0cxcfgcg0a5.northeurope-01.azurewebsites.net/api";
 const authenticate = (userForAuth) => {
   const request = axios.post(baseUrl, userForAuth);
   return request.then((response) => response);
