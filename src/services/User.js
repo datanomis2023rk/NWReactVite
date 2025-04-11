b6120a93aa4dbc6e737ccd9705f232703cd9f218
datanomis2023rk <rami.korkeila@student.careeria.fi>
2025-04-11 16:18:50 +0300

    backendin uusi osoite

--- src/services/User.js
+++ src/services/User.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 //const baseUrl = "https://localhost:7229/api/users";
 const baseUrl =
-  "https://northwindrestapi20250410122553-gsh7b5e5auccdecs.northeurope-01.azurewebsites.net";
+  "https://northwindrestapi-gmccb0cxcfgcg0a5.northeurope-01.azurewebsites.net";
 let token = null;
 // Tämä on metodi jota kutsutaan aina ennen kuin tehdään muu pyyntö serviceen
 // Parametrina annetaan token joka otetaan local storagesta
